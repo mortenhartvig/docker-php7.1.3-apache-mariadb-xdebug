712feabfcdfdd2b27af8d662a168fb55d2b87445
mortenhartvig <mh@rtvig.dk>
2017-03-24 00:20:55 +0100

    Create index.php
    Added example data. Replace content in this folder with your application files. This will be the document root in the "apache" docker container.

---
+++ wwwFiles/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Morten Hartvig <hartvigmorten@gmail.com>
+ * Date: 23-03-2017
+ * Click "Start listening for PHP Debug Connections" if using PhpStorm, and run the "docker-compose up" command.
+ * When viewing http://localhost, you should see your webserver, and if you have the xdebug plugin in e.g. Chrome, 
+ * a single click to enable + adding a breeakpoint (a red line - click the line number in front of a php event) will trigger the debugger successfully. 
+ */
+echo phpinfo();
